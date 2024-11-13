b306c6a6472d086f5479d56d5426b24af7bd2902
shraddha pandey <pandey200shraddha@gmail.com>
2024-11-13 07:18:15 +0530

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+    
+<title> Food ordar website - home page </title>
+<link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<body>
+    <!-- menu section starts -->
+     <div class= "menu"> 
+        <div class="wrapper">
+        Menu Gose Here 
+</div>
+     </div>
+     <!-- menu section ends -->
+
+     <!-- main content section starts -->
+     <div class= "main content"> 
+     <div class="wrapper">
+        Main Content Gose Here 
+</div>
+     </div>
+      <!-- main content section ends -->
+       <!-- footer section starts -->
+       <div class= "futter"> 
+       <div class="wrapper">
+        <p class="text-center"> 2020 All rights reserved. Designed By shraddha pandey </p>
+</div>
+     </div>
+        <!--footer section starts -->
+</body>
+
+</html>
